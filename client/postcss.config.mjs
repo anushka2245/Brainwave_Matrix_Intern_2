e3e9a8cfd059aed7436e46218c399b8c6f54b36f
anushka kushwaha <anushkakushwaha2245@gmail.com>
2025-04-23 18:16:03 +0530

    intial setup

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,5 +1,12 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
 export default config;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: ["@tailwindcss/postcss"]
+}
